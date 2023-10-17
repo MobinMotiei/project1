04c8f461b846b7b1656922b22b2c79f3d0e73188
Mobin <mobinmotiei@gmail.com>
2023-10-17 09:12:13 +0330

    add oop

---
+++ oop1.py
@@ -0,0 +1,18 @@
+class human:
+    #تابع پایینی یعنی شی ای دارم که اسم و غیره دارم
+    def __init__(self,name,lname,age):
+        #سلف یعنی اش مثلا سلف دات ایج یعنی سنش
+        self.name=name
+        self.lname=lname
+        self.age=age
+    def fun(self):
+        return f"{self.name} WELCOME!"
+        
+        
+ali=human("ali","mousavi",27)
+print(ali.fun())
+
+
+        
+   
+    
